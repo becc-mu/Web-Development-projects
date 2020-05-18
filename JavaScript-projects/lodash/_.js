144f405a44a71a08bb5fc3cafd33851f5c8e80b9
Rebecca Mulugeta <rebecca.mulugeta@ratedpeople.com>
2020-05-18 15:04:04 +0100

    passing inRange method

--- JavaScript-projects/lodash/_.js
+++ JavaScript-projects/lodash/_.js
@@ -4,6 +4,25 @@ const _ = {
     const clampedValue = Math.min(lowerClampedValue, upperBound);
     return clampedValue;
   },
+  inRange(number, start, end) {
+    let isInRange = false;
+    if (typeof end === "undefined") {
+      end = start;
+      start = 0;
+    }
+    if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    }
+    if (number >= start && number <= end) {
+      isInRange = true;
+    }
+    if (end === number) {
+      isInRange = false;
+    }
+    return isInRange;
+  },
 };
 
 // Do not write or modify code below this line.
