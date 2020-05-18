a9d4471325f7aa027cc59abc2f069993e7830f5a
Rebecca Mulugeta <rebecca.mulugeta@ratedpeople.com>
2020-05-18 12:38:18 +0100

    small steps, clamp method passed

--- JavaScript-projects/lodash/_.js
+++ JavaScript-projects/lodash/_.js
@@ -1,4 +1,10 @@
-const _ = {};
+const _ = {
+  clamp(number, lowerBound, upperBound) {
+    const lowerClampedValue = Math.max(number, lowerBound);
+    const clampedValue = Math.min(lowerClampedValue, upperBound);
+    return clampedValue;
+  },
+};
 
 // Do not write or modify code below this line.
 module.exports = _;
