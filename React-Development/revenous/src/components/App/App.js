c4dd34467180dfdc11fb848c845b39668a0eb905
Rebecca Mulugeta <rebecca.mulugeta@ratedpeople.com>
2020-06-09 18:07:59 +0100

    ravenous React app completed

---
+++ React-Development/revenous/src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+import '../SearchBar/SearchBar.css';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  );
+}
+
+export default App;
